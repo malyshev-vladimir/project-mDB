6756127632ee7a2d5b3a2178e754a9486d5f68cf
Malyshev Vova <malyshevvova@MBP-Malyshev.Dlink>
2020-07-28 04:57:14 +0300

    add changen in javascript-file

--- js/script.js
+++ js/script.js
@@ -24,3 +24,26 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+
+adv.forEach(item => {
+    item.remove();
+});
+
+document.getElementsByClassName('promo__genre')[0].textContent = "ДРАМА";
+
+document.getElementsByClassName('promo__bg')[0].style.backgroundImage = "url('img/bg.jpg')";
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
